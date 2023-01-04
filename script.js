5eca77149eb0d5723d52f8b37ce7542e43770f12
victorop <victordopr@gmail.com>
2023-01-04 07:37:49 -0300

    second commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//para criar o repositório git na pasta -> git init 
+//para que o arquivo comece a ser observado pelo git -> git add
+//git status -> mostra arquivos que tem git add -> necessário pra comitar
+//mostra a branch que estamos
+
+//git commit -m 'o que fiz de mudança' -> faz o commit, ou seja, cria um pacotinho com as mudanças
+
