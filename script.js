038f4e1e797ef70af6b1970021f75d88e500083c
victorop <victordopr@gmail.com>
2023-01-04 07:48:42 -0300

    atualização script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@
 
 //git commit -m 'o que fiz de mudança' -> faz o commit, ou seja, cria um pacotinho com as mudanças
 
+//git add -A => dá git add em todos os arquivos da pasta
+
+//git remote add origin linkrepositorio(no github) -> conecta o repositorio remoto ao github
+
+// depois dar git push -u origin  master 
+
+
+
